406c44829a0a4cc001179075c8fe64b72208d32e
benbackus <labalonso@Alonso-VEP.OPTNET.EDU>
2014-05-28 14:06:10 -0400

    Real color calibration data

--- HardwareParameters.m
+++ HardwareParameters.m
@@ -7,7 +7,7 @@ function [ HW ] = HardwareParameters()
     % Projector ('1424'), plasma ('1424plasma'), or stereoscope
     % ('1402chatnoir'), etc.
     HW.room = '1419';
-    HW.screenNum = 1; % see Screen('Screens?')
+    HW.screenNum = 2; % see Screen('Screens?')
     
     % HW.monWidth: width of entire viewable screen (cm)
     %   (Will later be multiplied by the fraction used, if stereoscope)
@@ -16,13 +16,13 @@ function [ HW ] = HardwareParameters()
     switch lower(HW.room)
         case '1419'
             switch HW.screenNum
-                case 1 % Experimental machine
+                case 1 % Control screen
+                    HW.useStereoscope = true;
+                case 2 % Experiment monitor
                     HW.monWidth = 38.5;
                     HW.viewDist = 68.5;
                     HW.useStereoscope = true;
                     knownRoom = true;
-                case 2 % Control screen
-                    HW.useStereoscope = true;
             end
         case '1424'
             switch HW.screenNum
@@ -143,6 +143,10 @@ function [ HW ] = HardwareParameters()
             HW.lumCalib = ...
                 importdata('media/lumCalib 1402chatnoir 2012-10-24.mat');
             HW.lumChannelContrib = [.2846 .5949 .1204];
+        case '1419'
+            HW.lumCalib = ...
+                importdata('media/lumCalib 1419 2014-05-27.mat');
+            HW.lumChannelContrib = [0.185506 0.743230 0.071263];
         otherwise
             warning('Parameters:NoColorCalib', ...
                 'No default color calibration data! Loading gamma = 2.0');
